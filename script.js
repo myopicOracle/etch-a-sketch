1012ae9a42a58624e14c6e7b027a05774eb4fb64
myopicOracle <184602237+myopicOracle@users.noreply.github.com>
2024-12-02 17:53:49 -0800

    Progress checkpoint: mouseover event works for a single element
    This is due to using querySelector instead of querySelectorAll, the
    former of which returns only the first match. Now I need to find
    out how I can take the NodeList generated by querySelectorAll,
    transform it into an array using Array.from(), and then select
    the individual elements of that array corresponding to the event.

--- script.js
+++ script.js
@@ -1,37 +1,21 @@
 // Create a 16 x 16 grid of div's using js only
 
-const bodyContainer = document.querySelector("body")
-
-const divContainer = document.getElementById("container")
-
-// const tile1 = document.createElement("div")
-// tile1.textContent = "O";
-// container.appendChild(tile1)
-
-// const tile2 = document.createElement("div")
-// container.appendChild(tile2)
-
+const container = document.getElementById("container")
 
     for (let i = 0 ; i < 16 ; i++) {
-        divContainer.appendChild(
-            document.createElement("div"))
+        // tileArray += container.appendChild(createTile);
+        // container.appendChild(document.createElement("div"))
+        container.innerHTML += `<div class="tile"></div>`
     }
+    // end of grid construction
+
+// make tile change color when hover over
+
+const tileNodeList = document.querySelector("div.tile")
+
+//const tileArray = Array.from(tileNodeList)
 
 
-// const newTile = document.createElement("div")
-
-// container.appendChild()
-
-
-
-
-
-// function addTiles() {
-//     let tileGrid;
-//     for (let i = 0 ; i < 16 ; i++) {
-//         tileGrid = container.appendChild(document.createElement("div"))
-//     }
-//     return tileGrid;
-// }
-
-// addTiles()
+tileNodeList.addEventListener("mouseover", (event) => {
+    event.target.style.cssText = "background-color: blue";
+})
